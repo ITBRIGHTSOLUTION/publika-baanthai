22f065c7e6d81f62d49cf2d6442c21b171bdeb4a
Kee Hui <ahhui1320@gmail.com>
2026-01-30 14:00:23 +0800

    Update jommakan2026.vue

--- pages/jommakan2026.vue
+++ pages/jommakan2026.vue
@@ -76,6 +76,9 @@
         </div>
       </div>
 
+      <!-- Meta Pixel noscript fallback -->
+      <noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1275477237747041&ev=PageView&noscript=1" /></noscript>
+
       <!-- Footer -->
       <footer class="w-full py-5 text-center text-[#333333] font-['Montserrat'] text-sm sm:text-base">
         © 2026 All rights reserved by Baan Thai Malaysia
@@ -93,6 +96,12 @@ export default {
   head() {
     return {
       title: 'Jom Makan - Baan Thai',
+      script: [
+        {
+          innerHTML: `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window, document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init', '1275477237747041');fbq('track', 'PageView');`,
+          type: 'text/javascript'
+        }
+      ]
     };
   },
 };
